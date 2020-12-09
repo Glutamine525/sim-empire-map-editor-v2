1e33bb2ec658b9cf5481f26aea79cdd6b4e472a3
Glutamine525 <az009931@vip.qq.com>
2020-12-09 18:37:33 +0800

    side nav appearance

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// var vm = new Vue({
-//     el: "#app",
-//     data: function () {
-//         return { visible: false };
-//     },
-// });
-
 var $length = 116;
 var $cellSize = 30;
 
@@ -12,7 +5,7 @@ var $topNav = new TopNav(40);
 var $sideNav = new SideNav(64);
 var $config = new Config();
 
-const resizeObserver = new ResizeObserver((entries) => {
+const topNavResizeObserver = new ResizeObserver((entries) => {
     let height = 0;
     for (let entry of entries) {
         height = entry.contentRect.height;
@@ -24,24 +17,40 @@ const resizeObserver = new ResizeObserver((entries) => {
     $sideNav.setMaginTop(height);
     $config.topNavHeight = height;
 });
-resizeObserver.observe($$("top-nav-height-helper"));
+topNavResizeObserver.observe($$("top-nav-height-helper"));
 
 onClickDisplayMode(true);
 drawBoundary("black");
 drawCell(getColor("--color-border-lighter"), getColor("--color-background-lighter"));
 
 if ("backdrop-filter" in document.documentElement.style) {
+    // if (false) {
     $config.isFtosted = true;
     let topNav = $$("top-nav");
     topNav.style.background = "transparent";
-    topNav.style.backdropFilter = "blur(20px)";
+    topNav.style.backdropFilter = "blur(15px)";
     topNav.style.margin = "4px";
     topNav.style.borderRadius = "20px";
     topNav.style.border = "1px solid var(--color-border-darker)";
     topNav.style.width = "calc(100% - 10px)";
+    $$("side-nav").style.background = "transparent";
+    let sideMenu = $$$("#side-nav .el-menu");
+    sideMenu.style.width = "56px";
+    sideMenu.style.margin = "0 0 4px 4px";
+    sideMenu.style.border = "1px solid var(--color-border-darker)";
+    sideMenu.style.borderRadius = "20px";
+    sideMenu.style.backdropFilter = "blur(15px)";
+    $$$("#side-nav .el-submenu__title", true).forEach((v) => {
+        v.style.borderRadius = "20px";
+    });
+    $$$("#side-nav .el-menu-item", true).forEach((v) => {
+        v.style.borderRadius = "20px";
+    });
     $topNav.setHeight($config.height);
     $config.topNavHeight += 8;
     $sideNav.setMaginTop($config.height);
 } else {
     $$("top-nav").style.background = "var(--color-background-darker)";
+    $$("side-nav").style.background = "var(--color-background-darker)";
+    $$("side-nav").style.boxShadow = "3px 3px 5px var(--color-border-base)";
 }
--- js/side-nav.js
+++ js/side-nav.js
@@ -15,6 +15,138 @@ class SideNav {
     }
 
     setMaginTop(top) {
-        $$(this.id).style.marginTop = `${top}px`;
+        $$(this.id).style.setProperty("--margin-top", `${top}px`);
     }
 }
+
+var vm = new Vue({
+    el: "#app",
+    data() {
+        return {
+            labelText: LabelText,
+            buildingCatagory: LabelText.building_catagory,
+            operationCatagory: LabelText.operation_catagory,
+            backgroundColor: "",
+            textColor: "",
+            activeTextColor: "",
+            // civilBuildingMap: {
+            //     China: BuildingChina,
+            //     Persian: BuildingPersian,
+            //     Egypt: BuildingEgypt,
+            //     Greece: BuildingGreece,
+            //     Aztaka: BuildingAztaka,
+            // },
+            buildingInfo: {
+                住宅: [],
+                农业: [],
+                工业: [],
+                商业: [],
+                市政: [],
+                文化: [],
+                宗教: [],
+                军事: [],
+                美化: [],
+                奇迹: [],
+                通用: [],
+            },
+        };
+    },
+    computed: {
+        operationCatagory1() {
+            let that = this;
+            return this.operationCatagory.filter(function (v) {
+                return v in that.labelText;
+            });
+        },
+        operationCatagory2() {
+            let that = this;
+            return this.operationCatagory.filter(function (v) {
+                return !(v in that.labelText);
+            });
+        },
+    },
+    methods: {
+        onClickDarkMode(isDarkMode) {
+            if (isDarkMode) {
+                this.backgroundColor = DarkMode["color-background-base"];
+                this.textColor = DarkMode["color-primary-text"];
+                this.activeTextColor = DarkMode["color-primary"];
+            } else {
+                this.backgroundColor = LightMode["color-background-base"];
+                this.textColor = LightMode["color-primary-text"];
+                this.activeTextColor = LightMode["color-primary"];
+            }
+        },
+        onChangeCivil(civil) {
+            let that = this;
+            Object.keys(this.buildingInfo).map(function (v) {
+                that.buildingInfo[v] = that.civilBuildingMap[civil][v];
+            });
+        },
+        onSelect(index, indexPath) {
+            if (indexPath[0] in this.buildingInfo) {
+                this.onSelectBuilding(index, indexPath);
+            } else {
+                this.onSelectOperation(index, indexPath);
+            }
+        },
+        onSelectOperation(index, indexPath) {
+            switch (index) {
+                case "道路":
+                    let newHolding = {};
+                    newHolding.text = "";
+                    newHolding.width = 1;
+                    newHolding.height = 1;
+                    newHolding.range = 0;
+                    newHolding.color = "var(--color-black)";
+                    newHolding.background = BuildingFixed.color_road;
+                    newHolding.borderColor = "var(--color-border-base)";
+                    newHolding.borderWidth = 1;
+                    newHolding.isProtection = false;
+                    newHolding.isRoad = true;
+                    newHolding.isPreview = true;
+                    Vue.prototype.operation = "placing-building";
+                    Vue.prototype.holding = newHolding;
+                    Vue.prototype.holdingSession = new Date().getTime();
+                    this.$emit("update:select-building", ["道路"]);
+                    break;
+                case "取消操作":
+                    Vue.prototype.operation = "null";
+                    Vue.prototype.holding = {};
+                    this.$emit("update:select-building", ["无"]);
+                    break;
+                case "删除建筑":
+                    Vue.prototype.operation = "deleting-building";
+                    Vue.prototype.holding = {};
+                    this.$emit("update:select-building", ["删除建筑"]);
+                    break;
+                default:
+                    console.log(index, indexPath);
+                    break;
+            }
+        },
+        onSelectBuilding(index, indexPath) {
+            let newHolding = {};
+            let selectedBuilding = this.getBuildingInfo(this.civil, indexPath[0], index);
+            newHolding.text = selectedBuilding.text;
+            newHolding.width = selectedBuilding.size;
+            newHolding.height = selectedBuilding.size;
+            newHolding.range = selectedBuilding.range_size;
+            newHolding.color = selectedBuilding.color;
+            newHolding.background = selectedBuilding.background_color;
+            newHolding.borderColor = selectedBuilding.border_color;
+            newHolding.borderWidth = 1;
+            newHolding.isPreview = true;
+            newHolding.isProtection = LabelText.protection_building[this.civil].indexOf(selectedBuilding.text) > -1;
+            Vue.prototype.operation = "placing-building";
+            Vue.prototype.holding = newHolding;
+            Vue.prototype.holdingSession = new Date().getTime();
+            this.$emit("update:select-building", indexPath);
+        },
+        getBuildingInfo(civil, catagory, name) {
+            return this.civilBuildingMap[civil][catagory].filter(function (v) {
+                return v.name === name;
+            })[0];
+        },
+    },
+});
